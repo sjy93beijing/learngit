5db24eda4dc2300799a96a5b40fc23d699419642
shengjunyang <shengjunyang@maoyan.com>
2019-07-25 17:23:54 +0800

    first push

---
+++ study_zhengze/src/main/java/com/maoyan/Zhengze.java
@@ -0,0 +1,11 @@
+package com.maoyan;
+
+/**
+ * @author sjy on 2019-07-25
+ * @version 1.0
+ */
+public class Zhengze {
+    public static void main(String[] args) {
+        System.out.println("sasa");
+    }
+}
